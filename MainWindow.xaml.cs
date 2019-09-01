cd6490475a4f65da59ff13152c7af1b875fe6fab
minhncedutw <minhnc.edu.tw@gmail.com>
2019-09-01 10:21:20 +0800

    send data OK

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.Ports;
+
+namespace SerialCommunication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort = new SerialPort();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {            
+            string[] ports = SerialPort.GetPortNames();
+            //Console.WriteLine("The following serial ports were found:");            
+            foreach (string port in ports)
+            {
+                //Console.WriteLine(port); // Display each port name to the console.
+                cBoxComPort.Items.Add(port);
+            }
+        }
+
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                serialPort.PortName = cBoxComPort.Text;
+                serialPort.BaudRate = Convert.ToInt32(cBoxBaudRate.Text);
+                serialPort.DataBits = Convert.ToInt32(cBoxDataBits.Text);
+                serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cBoxStopBits.Text);
+                serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), cBoxParityBits.Text);
+                serialPort.Open(); // Open port.
+
+                pBar.Value = 100;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close(); // Close port.
+
+                pBar.Value = 0;
+            }
+        }
+
+        private void BtnSendData_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Write(tBoxOutData.Text);
+            }
+        }
+    }
+}
